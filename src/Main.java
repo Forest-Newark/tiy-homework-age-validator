0f19a86385afd6327066af54137c19bb17a19264
Forest Newark <forest.newark@gmail.com>
2017-03-20 16:28:46 -0400

    main commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by forestnewark on 3/20/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Please enter your age: ");
+
+            if (Validator.validateAge(sc.next())) {
+                System.out.println("Age Accepted");
+            }
+
+
+        }
+
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Terminating Program");
+        }
+
+
+    }
+}
---
+++ src/Validator.java
@@ -0,0 +1,37 @@
+/**
+ * Created by forestnewark on 3/20/17.
+ */
+public class Validator {
+
+
+    public static boolean validateAge(String str) throws Exception {
+        double i;
+        int x;
+        try {
+            i = Double.parseDouble(str);
+            try {
+                 x = Integer.parseInt(str);
+            }catch (Exception e) {
+                System.out.println("Age must be an integer");
+                return false;
+            }
+
+        }catch (Exception e) {
+            System.out.println("Age must be a number.");
+            return false;
+        }
+
+        if (x > 115) {
+            throw new Exception("You appear to be lying about your age.");
+
+        }
+        if (x < 18) {
+            throw new Exception("You must be over 18.");
+        }
+
+        return true;
+
+    }
+
+
+}
